ceb7b66d546d328f8e057b76edd1cb0cb911cf96
faberlissss <maria.simochka@gmail.com>
2023-12-26 23:23:55 +0200

    write-script-average-numbers

---
+++ script.js
@@ -0,0 +1,6 @@
+let one = Number(prompt('Please enter the first number'));
+let two = Number(prompt('Please enter a second number'));
+let three = Number(prompt('Please enter the third number'));
+let sum = one + two + three;
+let divide = sum / 3;
+alert(divide);
